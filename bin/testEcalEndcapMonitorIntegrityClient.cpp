63fe161bce1de254acf4c49908a7a9ddd97896b3
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2007-04-02 16:19:51 +0000

    - first commit

---
+++ bin/testEcalEndcapMonitorIntegrityClient.cpp
@@ -0,0 +1,318 @@
+/*
+ * \file EcalEndcapMonitorIntegrityClient.cpp
+ *
+ *  $Date: 2006/07/07 18:41:49 $
+ *  $Revision: 1.27 $
+ *  \author G. Della Ricca
+ *
+ */
+
+#include "DQMServices/Core/interface/MonitorElement.h"
+#include "DQMServices/UI/interface/MonitorUIRoot.h"
+
+#include "DataFormats/EcalRawData/interface/EcalRawDataCollections.h"
+
+#include "DQM/EcalEndcapMonitorClient/interface/EEMUtilsClient.h"
+
+#include "TROOT.h"
+#include "TApplication.h"
+#include "TThread.h"
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+TCanvas* c1;
+TCanvas* c2;
+TCanvas* c3;
+
+MonitorUserInterface* mui;
+
+bool exit_now = false;
+bool exit_done = false;
+
+void *pth1(void *) {
+
+  bool stay_in_loop = true;
+
+  // last time monitoring objects were plotted
+  int last_plotting = -1;
+
+  while ( stay_in_loop && ! exit_now ) {
+
+    // this is the "main" loop where we receive monitoring
+    stay_in_loop = mui->update();
+
+    // subscribe to new monitorable matching pattern
+    mui->subscribeNew("*/EcalEndcap/EcalInfo/STATUS");
+    mui->subscribeNew("*/EcalEndcap/EcalInfo/RUN");
+    mui->subscribeNew("*/EcalEndcap/EcalInfo/EVT");
+    mui->subscribeNew("*/EcalEndcap/EcalInfo/RUNTYPE");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/Gain/EEIT gain SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/ChId/EEIT ChId SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/TTId/EEIT TTId SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/TTBlockSize/EEIT TTBlockSize SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/EEIT DCC size error");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemChId/EEIT MemChId SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemGain/EEIT MemGain SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemTTId/EEIT MemTTId SM01");
+    mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemSize/EEIT MemSize SM01");
+
+    // # of full monitoring cycles processed
+    int updates = mui->getNumUpdates();
+
+    MonitorElement* me;
+
+    string s;
+    string status;
+    string run;
+    string evt;
+    string type;
+
+    // draw monitoring objects every monitoring cycle
+    if ( updates != last_plotting ) {
+
+      me = mui->get("Collector/FU0/EcalEndcap/EcalInfo/STATUS");
+      if ( me ) {
+        s = me->valueString();
+        status = "unknown";
+        if ( s.substr(2,1) == "0" ) status = "start-of-run";
+        if ( s.substr(2,1) == "1" ) status = "running";
+        if ( s.substr(2,1) == "2" ) status = "end-of-run";
+        cout << "status = " << status << endl;
+      }
+
+      me = mui->get("Collector/FU0/EcalEndcap/EcalInfo/RUN");
+      if ( me ) {
+        s = me->valueString();
+        run = s.substr(2,s.length()-2);
+        cout << "run = " << run << endl;
+      }
+
+      me = mui->get("Collector/FU0/EcalEndcap/EcalInfo/EVT");
+      if ( me ) {
+        s = me->valueString();
+        evt = s.substr(2,s.length()-2);
+        cout << "event = " << evt << endl;
+      }
+
+      me = mui->get("Collector/FU0/EcalEndcap/EcalInfo/RUNTYPE");
+      if ( me ) {
+        s = me->valueString();
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::COSMIC ) type = "COSMIC";
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::LASER_STD ) type = "LASER";
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::PEDESTAL_STD ) type = "PEDESTAL";
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::TESTPULSE_MGPA ) type = "TEST_PULSE";
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::BEAMH4 ) type = "BEAMH4";
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::BEAMH2 ) type = "BEAMH2";
+        if ( atoi(s.substr(2,s.size()-2).c_str()) == EcalDCCHeaderBlock::MTCC ) type = "MTCC";
+        cout << "type = " << type << endl;
+      }
+
+      TH1F* h1;
+      TH2F* h2;
+
+      //      me = mui->get("Collector/FU0/EcalEndcap/EEIntegrityTask/EEIT DCC size error");
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/EEIT DCC size error");
+      h1 = EEMUtilsClient::getHisto<TH1F*>(me);
+      if ( h1 ) {
+        c1->cd();
+        h1->SetOption("text");
+        h1->Draw();
+        c1->Update();
+      }
+    
+      //      me = mui->get("Collector/FU0/EcalEndcap/EEIntegrityTask/Gain/EEIT gain SM01");
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/Gain/EEIT gain SM01");
+      h2 = EEMUtilsClient::getHisto<TH2F*>(me);
+      if ( h2 ) {
+        c2->cd(1);
+        h2->SetOption("text");
+        h2->Draw();
+        c2->Update();
+      }
+
+      //      me = mui->get("Collector/FU0/EcalEndcap/EEIntegrityTask/ChId/EEIT ChId SM01");
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/ChId/EEIT ChId SM01");
+      h2 = EEMUtilsClient::getHisto<TH2F*>(me);
+      if ( h2 ) {
+        c2->cd(2);
+        h2->SetOption("text");
+        h2->Draw();
+        c2->Update();
+      }
+
+      //      me = mui->get("Collector/FU0/EcalEndcap/EEIntegrityTask/TTId/EEIT TTId SM01");
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/TTId/EEIT TTId SM01");
+      h2 = EEMUtilsClient::getHisto<TH2F*>(me);
+      if ( h2 ) {
+        c2->cd(3);
+        h2->SetOption("text");
+        h2->Draw();
+        c2->Update();
+      }
+
+      //      me = mui->get("Collector/FU0/EcalEndcap/EEIntegrityTask/TTBlockSize/EEIT TTBlockSize SM01");
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/TTBlockSize/EEIT TTBlockSize SM01");
+      h2 = EEMUtilsClient::getHisto<TH2F*>(me);
+      if ( h2 ) {
+        c2->cd(4);
+        h2->SetOption("text");
+        h2->Draw();
+        c2->Update();
+      }
+
+      c2->cd();
+      c2->Modified();
+      c2->Update();
+
+      // me = mui->get("EcalEndcap/Sums/EEIntegrityTask/TTBlockSize/EEIT TTBlockSize SM01");
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/MemChId/EEIT MemChId SM01");
+       h2 = EEMUtilsClient::getHisto<TH2F*>(me);
+       if ( h2 ) {
+         c3->cd(1);
+         h2->SetOption("col");
+         h2->Draw();
+         c3->Update();
+       }
+
+      c3->cd();
+      c3->Modified();
+      c3->Update();
+
+      me = mui->get("EcalEndcap/Sums/EEIntegrityTask/MemGain/EEIT MemGain SM01");
+      h2 = EEMUtilsClient::getHisto<TH2F*>(me);
+      if ( h2 ) {
+        c3->cd(2);
+        h2->SetOption("col");
+        h2->Draw();
+        c3->Update();
+      }
+
+      c3->cd();
+      c3->Modified();
+      c3->Update();
+
+      last_plotting = updates;
+    }
+
+  }
+
+  exit_done = true;
+
+  return 0;
+}
+
+
+
+
+int main(int argc, char** argv) {
+
+  cout << endl;
+  cout << " *** Ecal Barrel Monitor Integrity Client ***" << endl;
+  cout << endl;
+
+  TApplication app("app", &argc, argv);
+
+  // default client name
+  string cfuname = "UserIntegrity";
+
+  // default collector host name
+  string hostname = "localhost";
+
+  // default port #
+  int port_no = 9090;
+
+  c1 = new TCanvas("Ecal Barrel Integrity Monitoring 1","Ecal Barrel Integrity Monitoring 1",  0, 0,400,400);
+  c1->Modified();
+  c1->Update();
+  c2 = new TCanvas("Ecal Barrel Integrity Monitoring 2","Ecal Barrel Integrity Monitoring 2",430, 0,600,600);
+  c2->Divide(2,2);
+  c2->Modified();
+  c2->Update();
+  c3 = new TCanvas("Ecal Barrel Integrity Monitoring 3","Ecal Barrel Integrity Monitoring 3", 430,615,600,370);
+  c3->Divide(1,2);
+  c3->Modified();
+  c3->Update();
+
+  if ( argc >= 2 ) cfuname = argv[1];
+  if ( argc >= 3 ) hostname = argv[2];
+  if ( argc >= 4 ) port_no = atoi(argv[3]);
+
+  cout << " Client " << cfuname
+       << " begins requesting monitoring from host " << hostname << endl;
+
+  // start user interface instance
+  mui = new MonitorUIRoot(hostname, port_no, cfuname);
+
+  mui->setVerbose(1);
+
+  // will attempt to reconnect upon connection problems (w/ a 5-sec delay)
+  mui->setReconnectDelay(5);
+
+  // subscribe to all monitorable matching pattern
+  mui->subscribe("*/EcalEndcap/EcalInfo/STATUS");
+  mui->subscribe("*/EcalEndcap/EcalInfo/RUN");
+  mui->subscribe("*/EcalEndcap/EcalInfo/EVT");
+  mui->subscribe("*/EcalEndcap/EcalInfo/RUNTYPE");
+  mui->subscribe("*/EcalEndcap/EEIntegrityTask/Gain/EEIT gain SM01");
+  mui->subscribe("*/EcalEndcap/EEIntegrityTask/ChId/EEIT ChId SM01");
+  mui->subscribe("*/EcalEndcap/EEIntegrityTask/TTId/EEIT TTId SM01");
+  mui->subscribe("*/EcalEndcap/EEIntegrityTask/TTBlockSize/EEIT TTBlockSize SM01");
+  mui->subscribe("*/EcalEndcap/EEIntegrityTask/EEIT DCC size error");
+  mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemChId/EEIT MemChId SM01");
+  mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemGain/EEIT MemGain SM01");
+  mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemTTId/EEIT MemTTId SM01");
+  mui->subscribeNew("*/EcalEndcap/EEIntegrityTask/MemSize/EEIT MemSize SM01");
+
+  CollateMonitorElement* cme;
+  
+  cme = mui->collate2D("EEIT gain SM01", "EEIT gain SM01", "EcalEndcap/Sums/EEIntegrityTask/Gain");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/Gain/EEIT gain SM01");
+
+  cme = mui->collate2D("EEIT ChId SM01", "EEIT ChId SM01", "EcalEndcap/Sums/EEIntegrityTask/ChId");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/ChId/EEIT ChId SM01");
+
+  cme = mui->collate2D("EEIT TTId SM01", "EEIT TTId SM01", "EcalEndcap/Sums/EEIntegrityTask/TTId");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/TTId/EEIT TTId SM01");
+
+  cme = mui->collate2D("EEIT TTBlockSize SM01", "EEIT TTBlockSize SM01", "EcalEndcap/Sums/EEIntegrityTask/TTBlockSize");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/TTBlockSize/EEIT TTBlockSize SM01");
+
+  cme = mui->collate1D("EEIT DCC size error", "DCC size error", "EcalEndcap/Sums/EEIntegrityTask");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/EEIT DCC size error");
+
+  cme = mui->collate2D("EEIT MemChId SM01", "EEIT MemChId SM01", "EcalEndcap/Sums/EEIntegrityTask/MemChId");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/MemChId/EEIT MemChId SM01");
+
+  cme = mui->collate2D("EEIT MemGain SM01", "EEIT MemGain SM01", "EcalEndcap/Sums/EEIntegrityTask/MemGain");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/MemGain/EEIT MemGain SM01");
+
+  cme = mui->collate2D("EEIT MemTTId SM01", "EEIT MemTTId SM01", "EcalEndcap/Sums/EEIntegrityTask/MemTTId");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/MemTTId/EEIT MemTTId SM01");
+
+  cme = mui->collate2D("EEIT MemSize SM01", "EEIT MemSize SM01", "EcalEndcap/Sums/EEIntegrityTask/MemSize");
+  mui->add(cme, "*/EcalEndcap/EEIntegrityTask/MemSize/EEIT MemSize SM01");
+
+
+  TThread *th1 = new TThread("th1",pth1);
+
+  th1->Run();
+
+  try { app.Run(kTRUE); } catch (...) { throw; }
+
+  mui->unsubscribe("*");
+
+  exit_now = true;
+
+  while ( ! exit_done ) { usleep(100); }
+
+  th1->Delete();
+
+  delete mui;
+
+  return 0;
+
+}
+
